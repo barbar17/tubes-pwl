78c5385e224181def752482154ae119523b85ab5
barbar17 <akbarmaulana644@gmail.com>
2022-12-22 06:30:19 +0700

    update pages

--- frontend/src/pages/Payment/Payment.js
+++ frontend/src/pages/Payment/Payment.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
+import { BsPersonCircle } from 'react-icons/bs';
 
 function Payment() {
   const navigate = useNavigate();
@@ -31,70 +32,74 @@ function Payment() {
   return (
     <div className="flex p-10">
       <div className="w-1/2">
-        <h3 className="ms-3 mb-4">HELLO WEDDING</h3>
-        <h6 className="mb-3">Information</h6>
-        <h5 className="mb-2">Contact Information</h5>
-        <div className="d-flex my-4 align-items-center">
-          <i className="bi bi-person-circle me-2" style={{ fontSize: "2rem" }}></i>
-          <h6>gilang@gmail.com</h6>
+        <div className="flex w-full justify-center items-center">
+          <h3 className="text-2xl tracking-widest">HELLO WEDDING</h3>
         </div>
-        <div className="row px-2">
-          <div className="col-6 p-1">
-            <input type="text" className="form-control" placeholder="name@example.com" />
-          </div>
-          <div className="col-6 p-1">
-            <input type="text" className="form-control" placeholder="name@example.com" />
-          </div>
-          <div className="col-6 p-1">
-            <input type="text" className="form-control" placeholder="name@example.com" />
+        <div>
+          <h6 className="my-5">Information</h6>
+          <h5 className="text-2xl">Contact Information</h5>
+          <div className="flex space-x-2 items-center">
+            <BsPersonCircle className="text-4xl" />
+            <h6>Gilang (gilang@gmail.com)</h6>
           </div>
         </div>
 
-        <div className="row px-2">
-          <div className="col-12 p-1 py-2">
-            <select className="form-select" aria-label="Default select example">
+        <div className="grid grid-cols-[repeat(2,250px)] px-2 py-8">
+          <div className="col-6 p-1">
+            <label>First Name</label>
+            <input type="text" className='border ml-2 border-slate-200 rounded-md text-lg p-2' placeholder="name@example.com" />
+          </div>
+          <div className="col-6 p-1">
+            <label>Last Name</label>
+            <input type="text" className='border ml-2 border-slate-200 rounded-md text-lg p-2' placeholder="name@example.com" />
+          </div>
+          <div className="col-span-2 p-1">
+            <label>Mobile Phone</label>
+            <input type="text" className='w-full border ml-2 border-slate-200 rounded-md text-lg p-2' placeholder="Ex: 081234567890" />
+          </div>
+
+
+          <div className="col-span-2 py-1 px-1">
+            <label>Package</label>
+            <select className="border ml-2 w-full border-slate-200 rounded-md text-lg p-2" aria-label="Default select example">
+              <option value="1">Wedding</option>
+              <option value="2">Prewedding</option>
+              <option value="3">Engagement</option>
+            </select>
+          </div>
+
+          <div className="col-span-2 py-1 px-1">
+            <label>Package Detail</label>
+            <select className="border ml-2 w-full border-slate-200 rounded-md text-lg p-2" aria-label="Default select example">
+              <option value="1">Silver</option>
+              <option value="2">Gold</option>
+              <option value="3">Diamond</option>
+            </select>
+          </div>
+
+          <div className="col-span-2 py-1 px-1">
+            <label>Adds On</label>
+            <select className="border ml-2 w-full border-slate-200 rounded-md text-lg p-2" aria-label="Default select example">
               <option>Open this select menu</option>
               <option value="1">One</option>
               <option value="2">Two</option>
               <option value="3">Three</option>
             </select>
           </div>
-          <div className="col-12 p-1 py-2">
-            <select className="form-select" aria-label="Default select example">
-              <option>Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
+
+          <div className="col-span-2 py-1 px-1">
+            <label>Date & Time Reservation</label>
+            <input className="border ml-2 w-full border-slate-200 rounded-md text-lg p-2" aria-label="Default select example" type={'datetime-local'} />
           </div>
-          <div className="col-12 p-1 py-2">
-            <select className="form-select" aria-label="Default select example">
-              <option>Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </div>
-          <div className="col-12 p-1 py-2">
-            <select className="form-select" aria-label="Default select example">
-              <option>Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </div>
-          <div className="col-12 p-1 py-2">
-            <select className="form-select" aria-label="Default select example">
-              <option>Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
+
+          <div className="col-span-2 py-1 px-1">
+            <label>Location</label>
+            <input type="text" className='w-full border ml-2 border-slate-200 rounded-md text-lg p-2' placeholder="Ex: Monas" />
           </div>
         </div>
 
-        <div className="d-flex my-5">
-          <button type="button" className="btn btn-light btn-lg ms-auto" style={{ backgroundColor: "#D7DBDD" }}>
+        <div className="flex w-full justify-end">
+          <button type="button" className="py-2 px-3 text-xl rounded-md" style={{ backgroundColor: "#D7DBDD" }}>
             Continue
           </button>
         </div>
--- frontend/src/pages/Prewedding/Prewedding.js
+++ frontend/src/pages/Prewedding/Prewedding.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Prewedding() {
     return (
-        <div className='w-full flex flex-col items-center p-10'>
-            <h1 className='text-7xl text-gray-500'>Pre Wedding Journal</h1>
+        <div className='flex flex-col items-center p-10'>
+            <h1 className='text-7xl text-gray-500'>Prewedding Journal</h1>
             <div className='w-4/5 shadow-md flex border-2 border-gray-200 my-10 p-10 flex-col justify-center items-center'>
-                <img src='grid2.jpg' alt='fotowedding' className='w-11/12 object-cover' />
+                <img src='AGZ_1411.png' alt='fotowedding' className='w-11/12 object-cover' />
                 <div className='flex text-center w-full py-5'>
                     <div className='flex flex-col w-1/5 justify-center items-center'>
                         <span className='text-xl'>15</span>
@@ -19,6 +19,28 @@ function Prewedding() {
                         <span className='text-lg text-gray-400 pt-5'><i>READ MORE</i></span>
                     </div>
                 </div>
+            </div>
+
+            <div className='w-4/5'>
+                <div className='flex flex-col w-full items-center my-5'>
+                    <span className='text-2xl'>A SOULFUL RENDEZVOUZ</span>
+                    <hr className='w-3/5 border-2 my-2' />
+                    <span className='text-lg text-gray-400'>ARUM & REZA WEDDING BY HELLO WEDDING</span>
+                </div>
+
+                <div className='w-full mb-10'>
+                    <img src='AGZ_1411.png' alt='wedding2' className='h-[650px] w-full object-cover mb-10' />
+                    <span className='text-xl'><i>Story about this wedding</i></span>
+                </div>
+            </div>
+
+            <div className='w-full px-10 my-10'>
+                <img src='photo3.jpg' alt='wedding4' className='w-full h-screen object-cover' />
+            </div>
+
+            <div className='w-full px-10 space-x-4 flex my-10'>
+                <img src='grid7.jpg' alt='wedding4' className='w-1/2 h-screen object-cover' />
+                <img src='grid6.jpg' alt='wedding4' className='w-1/2 h-screen object-cover' />
             </div>
         </div>
     )
